b179bffc4cbbd9fbaefda4b691f099d620c61a7e
Swapnil Diliprao Joshi <72249773+swapniljoshi22@users.noreply.github.com>
2023-09-11 22:43:33 +0530

    Add files via upload

---
+++ Leet_Code/Pandas Problems/Example 1/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def total_time(employees: pd.DataFrame) -> pd.DataFrame:
+    
+    employees['time_spent'] = employees['out_time'] - employees['in_time']
+    
+    result_df = employees.groupby(['emp_id', 'event_day'])['time_spent'].sum().reset_index()
+    
+    result_df.rename(columns={'event_day': 'day', 'time_spent': 'total_time'}, inplace=True)
+    
+    return result_df
